0e5e54cd9ed26c4d53e817841d8a8e4a56d280fa
Loahit5101 <loahit5101@gmail.com>
2023-09-09 19:29:29 -0400

    add cpp version

---
+++ visual-odometry/CUDA_VO/build/CMakeFiles/vo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vo.dir/VO.cpp.o"
+  "vo"
+  "vo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
