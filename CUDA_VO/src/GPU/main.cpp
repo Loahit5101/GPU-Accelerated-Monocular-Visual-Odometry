e69c4e2863ac6f41f3657e8d3e7856c73231de52
Loahit Krishnamurthy <loahit5101@gmail.com>
2024-03-03 17:56:02 -0500

    wrapped into a VO class

--- CUDA_VO/src/GPU/main.cpp
+++ CUDA_VO/src/GPU/main.cpp
@@ -1,20 +1,27 @@
 
 #include "GPU/VO_class.h"
 
-int main(){
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        cout << "Usage: " << argv[0] << " <img_data_dir> <descriptor_type>" << endl;
+        return 1;
+    }
 
-    string img_data_dir = "/home/loahit/Downloads/Vis_Odo_project/09/image_0";
+    string img_data_dir = argv[1];
+    string descriptor_type = argv[2];
+
     vector<String> img_list;
     glob(img_data_dir + "/*.png", img_list, false);
     sort(img_list.begin(), img_list.end());
     int num_frames = img_list.size();
 
-    for (int i = 0; i < num_frames; ++i) { img_list[i].erase(img_list[i].begin() + 50); img_list[i].erase(img_list[i].begin() + 50);}
+    for (int i = 0; i < num_frames; ++i) {
+        img_list[i].erase(img_list[i].begin() + 50); // Assuming you want to erase characters at index 50 twice
+        img_list[i].erase(img_list[i].begin() + 50);
+    }
 
-    string s = "ORB_CPU";
-
-    VisualOdometry VO(s);
+    VisualOdometry VO(descriptor_type);
     VO.run_VO(img_list);
-    
+
     return 0;
 }
